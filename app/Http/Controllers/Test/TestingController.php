38aabae5b0d02dd4820d84aa51d462aae12c71a8
Ohis Lawrence <49398616+Ohislawrence@users.noreply.github.com>
2025-11-29 23:00:46 +0100

    bug fixed

--- app/Http/Controllers/Test/TestingController.php
+++ app/Http/Controllers/Test/TestingController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers\Test;
 
 use App\Http\Controllers\Controller;
-use App\Models\CourseOutline;
+use App\Models\CourseCreationReminder;
 use App\Models\Quiz;
 use App\Models\Subscription;
 use App\Models\SubscriptionPlan;
@@ -17,28 +17,7 @@ class TestingController extends Controller
         //$subscription = auth()->user()->current_subscription;
        // dd($subscription->hasFeature('unlimited_course_creation'));
         $student = auth()->user();
-        $course = $student->courses()
-                ->where('courses.id', 24)
-                ->where('status', 'active')
-                ->firstOrFail();
-       $availableOutlines = $course->modules()
-                ->with(['topics' => function ($query) {
-                    $query->orderBy('order');
-                }])
-                ->orderBy('order')
-                ->get()
-                ->flatMap(function ($module) {
-                    return $module->topics->map(function ($topic) use ($module) {
-                        return [
-                            'id' => $topic->id,
-                            'title' => $topic->title,
-                            'description' => $topic->description,
-                            'module_title' => $module->title,
-                            'full_title' => "{$module->title} → {$topic->title}",
-                        ];
-                    });
-                });
-        dd($availableOutlines );
-
+        $vart = CourseCreationReminder::find('1');
+        dd($vart);
     }
 }
