63417de44847ae59bdb4a1606ddb8908298cb113
Ohis Lawrence <49398616+Ohislawrence@users.noreply.github.com>
2026-01-12 19:41:48 +0100

    reminder done82

--- app/Policies/CapstoneProjectPolicy.php
+++ app/Policies/CapstoneProjectPolicy.php
@@ -11,13 +11,5 @@ class CapstoneProjectPolicy
 {
     use HandlesAuthorization;
 
-    public function view(User $user, CapstoneProject $capstoneProject)
-    {
-        return $capstoneProject->course->student_profile_id === $user->studentProfile->id;
-    }
 
-    public function update(User $user, CapstoneProject $capstoneProject)
-    {
-        return $capstoneProject->course->student_profile_id === $user->studentProfile->id;
-    }
 }
